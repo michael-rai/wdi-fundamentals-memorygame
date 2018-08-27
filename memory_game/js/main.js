3973d1250dba965fc430dfbf0c0d9125e7db9e76
Michael Rai <REBEL@Michaels-MBP.fios-router.home>
2018-08-27 00:52:06 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,19 @@
 console.log("Up and running!");
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log( "User flipped " + cardOne);
-var cardTwo = cards [2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) {
-	cardsInPlay === cardOne && cardTwo;
-}else if (cardsInPlay[0] === cardsInPlay[1]){
-	alert("You found a mach!")
-}else alert("Sorry, try again")
-	
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+}
+var flipCard = function(cardId) {
+	console.log("Card was flipped " + cards[cardId]);
+	push.cardId(cardsInPlay);
+	checkForMatch();
+
+}
+flipCard(0);
+flipCard(2);
+
