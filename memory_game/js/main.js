87e4f22f9b1ac23d265ed34f017116c26605aee3
Michael Rai <REBEL@Michaels-MacBook-Pro.local>
2018-08-28 20:27:45 -0400

    redid game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,15 +2,33 @@ console.log("Up and running!");
 //variables listed below
 var cards = ["Queen!","Queen!","King!","King!"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-//
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+//functions below
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+};
+};
+var flipCard = function (cardId) {
+//	var cardId = cards[0,1,2,3];
+//console.log("User flipped " + cardId);
+cardsInPlay.push(cards[cardId]); 
+if (cardsInPlay.length === 2) {
+	checkForMatch()
+}
+console.log("User flipped " + cards[cardId]);
+/*
+console.log("User flipped " + cardId);
+cardsInPlay.push(cards[cardId]);
+*/
+};
+flipCard(0);
+flipCard(2);
+
 if (cardsInPlay.length === 2) {
 
 }if (cardsInPlay[0] === cardsInPlay[1]){
 	alert("You found a match!");
-} else alert("Sorry, try again");
+} else {alert("Sorry, try again.")};
+
